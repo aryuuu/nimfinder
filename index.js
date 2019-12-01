61634886b7fd98d7303c2ab38249c492b0961fbd
aryuuu <herppratama@gmail.com>
2019-12-01 12:33:34 +0700

    syncing file from raspi

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+
+const express = require('express');
+const cors = require('cors');
+const mysql = require("mysql");
+const port = 6969;
+const dbname = process.env.DBNAME;
+const dbuname = process.env.DBUNAME;
+const dbpwd = process.env.DBPWD;
+
+const dbConn = mysql.createConnection({
+	host: 'localhost',
+	user: dbuname,
+	password: dbpwd,
+	database: dbname
+});
+
+
+const app = express();
+app.use(cors());
+app.use(express.static('public'));
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+	res.sendFile('./public/index.html');
+});
+
+app.get('/nims/:id', (req, res, next) => {
+
+	nama = tpb = s1 = s2 = s3 = '%' + req.params.id + '%';
+
+	dbConn.query("SELECT * FROM nim WHERE nama LIKE ? OR tpb LIKE ? OR s1 LIKE ? OR s2 LIKE ? OR s3 LIKE ?", 
+		[nama, tpb, s1, s2, s3],
+		(err, results, fields) => {
+			if (err) {
+				res.status(500).send({message: "something went wrong :("});
+			} else if (results.length > 0){
+				res.status(200).send({message: "we found something", count: results.length, data: results});
+			} else {
+				res.send({message: "nothing found :(", count : 0});
+			}
+		});
+
+});
+
+app.listen(port);
+console.log(`listening on port ${port}`);
